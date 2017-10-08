7ab5ad1874b1e83195a5c862210eaa858d84d9ab
vagrant <vagrant@vagrant.vm>
2017-10-08 23:53:32 +0000

    solved problem using set subtraction

--- missing.py
+++ missing.py
@@ -9,8 +9,19 @@ def missing_number(nums, max_num):
 
     >>> missing_number([7, 3, 2, 4, 5, 6, 1, 9, 10], 10)
     8
-    
     """
+
+    # convert max_num into set of numbers
+    max_num = range(max_num + 1)
+    max_num = set(max_num[1:])
+
+    # convert nums into set
+    nums = set(nums)
+
+    # do set subtraction to find missing #
+    missing_num = max_num - nums
+
+    return list(missing_num)[0]
 
 
 if __name__ == '__main__':
